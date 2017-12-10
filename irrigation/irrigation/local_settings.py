e6a1807cdc40ae6e7a73c943b0ca55bdea640971
jmiler1178 <jmiller1178@gmail.com>
2017-12-10 16:15:45 -0500

    exclude local_settings.py

--- irrigation/irrigation/local_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from django.conf import settings
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(settings.BASE_DIR, 'db/irrigation_rpi2.db'),
-    }
-}
