0d5f26c6b5122bac5d122a7457818b5f9a24b65c
Jeff Miller <jeffm@thestowcompany.com>
2020-01-26 10:52:07 -0500

    decode bytes

--- irrigation/sprinklesmart/views.py
+++ irrigation/sprinklesmart/views.py
@@ -259,7 +259,7 @@ def get_schedule(request,  scheduleId):
 @require_POST
 def toggle_zone(request):
     response = {}
-    json_data = json.loads(request.body)
+    json_data = json.loads(request.body.decode('utf-8'))
     zone_id = json_data['zoneId']
 
     # read the Zone info from the database
