d525386479edd0c91d4f07ee44dcea4d50147a9b
Jeff Miller <jeffm@thestowcompany.com>
2020-02-01 14:29:30 -0500

    signal on Zone post save

---
+++ irrigation/sprinklesmart/signals.py
@@ -0,0 +1,16 @@
+from models import Zone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import IntegrityError
+from django.contrib.sites.shortcuts import get_current_site
+from custom_apps.core.middleware import get_current_request, get_current_user
+from django.conf import settings
+from django.core.exceptions import MultipleObjectsReturned
+import logging
+
+logger = logging.getLogger(__name__)
+
+@receiver(post_save, sender=Zone)
+def post_save_Zone(sender, instance, *args, **kwargs):
+    zone = instance
+    logger.info("Zone Save {0}".format(zone.json))
