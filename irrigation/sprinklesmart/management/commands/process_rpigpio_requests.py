77a3a63fbae15519256b6fe90659be4b14a96ba8
Jeff Miller <jeffm@thestowcompany.com>
2020-02-02 16:17:43 -0500

    automatic mode required to schedule / execute RpiGpioRequest

--- irrigation/sprinklesmart/management/commands/process_rpigpio_requests.py
+++ irrigation/sprinklesmart/management/commands/process_rpigpio_requests.py
@@ -1,11 +1,12 @@
-from django.core.management.base import BaseCommand
-from django.shortcuts import get_object_or_404
 from datetime import datetime
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 from sprinklesmart.models import *
 from sprinklesmart.gpio.controller import *
 from django.db.models import Q
+
 import logging
-from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -13,77 +14,52 @@ class Command(BaseCommand):
     help = 'Process RPi GPIO Requests'
     
     def handle(self, *args, **options):
-        # logger.debug("process_rpigpio_request invoked")
-        irrigation_system = get_object_or_404(IrrigationSystem, pk=1)
+        irrigation_system = IrrigationSystem.objects.get(pk=1)
 
-        pending_status = get_object_or_404(Status, pk=1) # 1 is pending
-        complete_status = get_object_or_404(Status, pk=2) # 2 is complete
-        cancel_status = get_object_or_404(Status, pk=3) # 3 is cancel
-        active_status = get_object_or_404(Status, pk=4) # 4 is active
+        # are we in automatic mode and the system enabled
+        if irrigation_system.system_mode.automatic_mode and\
+            irrigation_system.systemState:
 
-        # get the current weatherconditions
-        current_weather = WeatherCondition.objects.order_by('-id')[0]
+            pending_status = get_object_or_404(Status, pk=1) # 1 is pending
+            complete_status = get_object_or_404(Status, pk=2) # 2 is complete
+            active_status = get_object_or_404(Status, pk=4) # 4 is active
 
-        # read the IrrigationSystem.systemState
-        
-        system_enabled = irrigation_system.systemState
-        
-        # if it is raining or the irrigation system is disabled
-        if current_weather.conditionCode.IsRaining() or not system_enabled:
-            # turn off 24VAC
-            turn_24_vac_off()
-            # turn off all other outputs
-            turn_all_outputs_off()
-            # turn off the indicator that the system is active - this is the blue LED
-            turn_irrigation_system_active_off()
-        else:
-            # otherwise
-            # turn on 24VAC
-            Turn24VACOn()
-        
-        twentyfour_vac_enabled = irrigation_system.system_enabled_zone.is_on
+            # get the current weatherconditions - used for determining if it is raining
+            current_weather = WeatherCondition.objects.order_by('-id')[0]
 
-        if system_enabled and twentyfour_vac_enabled:
-            # system is completely enabled
-            # logger.debug("Irrigation System is Enabled")
-            
+            # if it is raining or the irrigation system is disabled
+            if current_weather.conditionCode.IsRaining():
+                # turn off 24VAC
+                turn_24_vac_off()
+            else:
+                # turn on 24VAC
+                turn_24_vac_on()
+
             current_time = datetime.now()
             match_time = datetime(current_time.year, current_time.month, current_time.day, current_time.hour, current_time.minute, second=0, microsecond=0)
                         
             # are there any active requests whith the off time equal to now (to the minute)
-            active_requests = RpiGpioRequest.objects.filter(status=active_status, offDateTime=match_time)
-            # logger.debug("active requests count {0}".format(active_requests.count()))
+            off_requests = RpiGpioRequest.objects.filter(status=active_status, offDateTime=match_time)
+
             # if there are turn off the output
-            if active_requests.count() > 0:
-                for active_request in active_requests:
-                    outputRpiGpioCommand(active_request.rpiGpio, Commands.OFF)
-                    active_request.status = complete_status
-                    active_request.save()
-                    TurnIrrigationSystemActiveOff()
+            if off_requests.count() > 0:
+                for off_request in off_requests:
+                    output_rpi_gpio_command(off_requests.rpiGpio, Commands.OFF)
+                    off_request.status = complete_status
+                    off_request.save()
                     
             # are there any pending requests with the on time equal to now (to the minute)
-            pending_requests = RpiGpioRequest.objects.filter(status=pending_status, onDateTime=match_time)
-            # logger.debug("pending_requests count {0}".format(pending_requests.count()))
+            pending_on_requests = RpiGpioRequest.objects.filter(status=pending_status, onDateTime=match_time)
 
             # if there are turn on the output
-            if pending_requests.count() > 0:
-                for pending_request in pending_requests:
-                    outputRpiGpioCommand(pending_request.rpiGpio, Commands.ON)
-                    pending_request.status = active_status
-                    pending_request.save()
-                    TurnIrrigationSystemActiveOn()
-                    
+            if pending_on_requests.count() > 0:
+                # turn on the indicator that we have valves enabled (aka Blue LED)
+                turn_irrigation_system_active_on()
+                for pending_on_request in pending_on_requests:
+                    output_rpi_gpio_command(pending_on_request.rpiGpio, Commands.ON)
+                    pending_on_request.status = active_status
+                    pending_on_request.save()
             else:
-                pending_request = RpiGpioRequest.objects.filter(status=pending_status).order_by('onDateTime').first()
-                # logger.debug("next pending_request {0}".format(pending_request))
-        else:
-            # logger.debug("Irrigation System is Disabled")
-            TurnAllOutputsOff()
-            TurnIrrigationSystemActiveOff() 
-            # cancel all requests which are active or pending
-            open_requests = RpiGpioRequest.objects.filter(Q(status=active_status) | Q(status=pending_status))
-            if open_requests.count() > 0:
-                for request in open_requests:
-                    request.status = cancel_status
-                    request.save()
-                    
+                # turn off the indicator that we have valves enabled (aka Blue LED)
+                turn_irrigation_system_active_off()
+             
--- irrigation/sprinklesmart/management/commands/schedule_irrigation_controller.py
+++ irrigation/sprinklesmart/management/commands/schedule_irrigation_controller.py
@@ -3,7 +3,8 @@ from django.core.management.base import BaseCommand, CommandError
 from sprinklesmart.api.weather import WeatherAPI
 from django.shortcuts import get_object_or_404
 from datetime import datetime, date, timedelta
-from sprinklesmart.models import RpiGpioRequest, Schedule, WeekDay, Status, WeatherCondition
+from sprinklesmart.models import IrrigationSystem, RpiGpioRequest, Schedule,\
+                                WeekDay, Status, WeatherCondition
 from django.db.models import Q
 import logging
 
@@ -13,62 +14,62 @@ class Command(BaseCommand):
     help = 'Schedule Irrigation Controller Events'
     
     def handle(self, *args, **options):
-        logger.debug("schedule_irrigation_controller invoked")
-        active_status = get_object_or_404(Status, pk=4) # 4 is active
-        pending_status = get_object_or_404(Status, pk=1) # 1 is pending
-        # Let's start by checking if ANYTHING is scheduled for today because, if there is, then we shouldn't
-        # have to execute any of the code below, right??
-        requests = RpiGpioRequest.objects.filter(Q(status=active_status) | Q(status=pending_status), onDateTime__contains=date.today())
+        irrigation_system = IrrigationSystem.objects.get(pk=1)
         
-        if requests.count() == 0:
-            # there is nothing scheduled for today so do the scheduling process
+        # are we in automatic mode and the system enabled
+        if irrigation_system.system_mode.automatic_mode and\
+            irrigation_system.systemState:
+
+            active_status = get_object_or_404(Status, pk=4) # 4 is active
+            pending_status = get_object_or_404(Status, pk=1) # 1 is pending
+
+            # Let's start by checking if ANYTHING is scheduled for today because, if there is, then we shouldn't
+            # have to execute any of the code below, right??
+            requests = RpiGpioRequest.objects.filter(Q(status=active_status) | Q(status=pending_status), onDateTime__contains=date.today())
+        
+            if requests.count() == 0:
+                # there is nothing scheduled for today so do the scheduling process
             
-            # look for active schedule(s) with a start time within the next hour
-            enabled_schedules = Schedule.objects.filter(enabled=True)
-            sprinkle_smart_multiplier = self.get_sprinkle_smart_multiplier()
+                # look for active schedule(s) with a start time within the next hour
+                enabled_schedules = Schedule.objects.filter(enabled=True)
+                sprinkle_smart_multiplier = self.get_sprinkle_smart_multiplier()
             
-            # retrieve IrrigationSchedule records for this day of the week and for the above Schedule 
-            current_time = datetime.now()
-            logger.debug("Current Time: {0}".format(current_time))
+                # retrieve IrrigationSchedule records for this day of the week and for the above Schedule
+                current_time = datetime.now()
+
+                # find the WeekDay object for today
+                week_day = get_object_or_404(WeekDay, weekDay=current_time.weekday())
             
-            # find the WeekDay object for today
-            week_day = get_object_or_404(WeekDay, weekDay=current_time.weekday())
-            pending_status = get_object_or_404(Status, pk=1)
-            
-            for schedule in enabled_schedules:
+                for schedule in enabled_schedules:
                 
-                schedule_time=datetime(current_time.year, current_time.month, current_time.day, schedule.startTime.hour, schedule.startTime.minute)
-                logger.debug("Enabled Schedule {0} Schedule Time {1}".format(schedule, schedule_time))
-                if schedule_time > current_time:
-                    delta_time=schedule_time-current_time
-                    minutes_in_future = (delta_time.seconds * sprinkle_smart_multiplier) / 60
-                    logger.debug("Minutes in Future {0}".format(minutes_in_future))
+                    schedule_time = datetime(current_time.year, current_time.month, current_time.day, schedule.startTime.hour, schedule.startTime.minute)
+                    if schedule_time > current_time:
+                        delta_time = schedule_time-current_time
+                        minutes_in_future = (delta_time.seconds * sprinkle_smart_multiplier) / 60
                     
-                    if minutes_in_future < 60:
-                        # we're less than an hour away from scheduled start time so need to stup the RpiGpioRequests per that schedule
-                        irrigation_schedules = schedule.irrigationschedule_set.filter(weekDays=week_day).order_by('sortOrder')
-                        zone_start_time = datetime(current_time.year, current_time.month, current_time.day, schedule.startTime.hour, schedule.startTime.minute)
-                        
-                        for irrigation_schedule in irrigation_schedules:
-                            # create RpiGpioRequest records for today
-                            # there should only be one rpiGpio per zone but it is a set so iterate anyway
-                            rpigpios = irrigation_schedule.zone.rpigpio_set.all()
+                        if minutes_in_future < 60:
+                            # we're less than an hour away from scheduled start time so need to stup the RpiGpioRequests per that schedule
+                            irrigation_schedules = schedule.irrigationschedule_set.filter(weekDays=week_day).order_by('sortOrder')
+                            zone_start_time = datetime(current_time.year, current_time.month, current_time.day, schedule.startTime.hour, schedule.startTime.minute)
+
+                            for irrigation_schedule in irrigation_schedules:
+                                # create RpiGpioRequest records for today
+                                # there should only be one rpiGpio per zone but it is a set so iterate anyway
+                                rpigpios = irrigation_schedule.zone.rpigpio_set.all()
                             
-                            for rpigpio in rpigpios:            
-                                        
-                                scheduled_request = RpiGpioRequest()
-                                scheduled_request.rpiGpio=rpigpio
-                                scheduled_request.onDateTime = zone_start_time
-                                
-                                duration_seconds = irrigation_schedule.duration * 60
-                                zone_end_time = zone_start_time + timedelta(0, duration_seconds)
-                                
-                                scheduled_request.offDateTime = zone_end_time
-                                scheduled_request.status = pending_status
-                                scheduled_request.durationMultiplier = sprinkle_smart_multiplier
-                                scheduled_request.save()
-                                logger.debug("Scheduled Request {0}".format(scheduled_request))
-                                zone_start_time = zone_end_time
+                                for rpigpio in rpigpios:            
+                                    scheduled_request = RpiGpioRequest()
+                                    scheduled_request.rpiGpio = rpigpio
+                                    scheduled_request.onDateTime = zone_start_time
+                                    
+                                    duration_seconds = irrigation_schedule.duration * 60
+                                    zone_end_time = zone_start_time + timedelta(0, duration_seconds)
+                                    
+                                    scheduled_request.offDateTime = zone_end_time
+                                    scheduled_request.status = pending_status
+                                    scheduled_request.durationMultiplier = sprinkle_smart_multiplier
+                                    scheduled_request.save()
+                                    zone_start_time = zone_end_time
     
     
     def get_sprinkle_smart_multiplier(self):
@@ -77,10 +78,12 @@ class Command(BaseCommand):
         weather_conditions = WeatherCondition.objects.filter(conditionDateTime__gt=datetime.now()-timedelta(days=2))
         total_count = weather_conditions.count()
         rain_count = 0
+        
         for weather_condition in weather_conditions:
             if weather_condition.conditionCode.IsRaining():
                 rain_count = rain_count + 1
-        if total_count > 0:        
+        
+        if total_count > 0:
             multiplier = 1.0 - rain_count / total_count
 
         return multiplier
