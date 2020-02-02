b488afb5bcf6a466231f28bcd0c59aea6497b0b8
Jeff Miller <jeffm@thestowcompany.com>
2020-02-02 09:53:17 -0500

    re-enable ping pong debug

--- irrigation/static/js/ws.js
+++ irrigation/static/js/ws.js
@@ -4,10 +4,10 @@ jQuery(document).ready(function ($) {
     var ws = new WebSocket('ws://' + rabbitmq_host + ':' + rabbitmq_ws_port + '/ws');
     var client = Stomp.over(ws);
 
-    client.debug = function () {
+    //client.debug = function () {
         // empty function to stop the PING, PONG printing
         // alternatively, we can make this do something different if we wanted...
-    };
+    //};
 
     var on_connect = function (x) {
         client.subscribe(rabbitmq_topic, on_message);
